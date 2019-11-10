47f1441f2754b7c3d6a8a8766b94decf245e138e
addyosmani <addyosmani@gmail.com>
2019-11-09 17:43:02 -0800

    [infra] Fix for now removed hook and bump package

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export { default as useNetworkStatus } from './network';
 export { default as useSaveData } from './save-data';
 export { default as useMemoryStatus } from './memory';
 export { default as useHardwareConcurrency } from './hardware-concurrency';
-export { default as useBatteryStatus } from './battery';
