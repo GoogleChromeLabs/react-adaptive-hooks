78539e5a540e71354e36e11969522431c8247538
Wing Leung <damianleung@gmail.com>
2020-03-15 17:19:55 +0100

    move supported outside of useNetworkStatus hook

--- network/network.test.js
+++ network/network.test.js
@@ -16,113 +16,134 @@
 
 import { renderHook, act } from '@testing-library/react-hooks';
 
-import { useNetworkStatus } from './';
+const map = {};
+
+const ectStatusListeners = {
+  addEventListener: jest.fn().mockImplementation((event, callback) => {
+    map[event] = callback;
+  }),
+  removeEventListener: jest.fn()
+};
+
+/**
+ * Tests that addEventListener or removeEventListener was called during the
+ * lifecycle of the useEffect hook within useNetworkStatus
+ */
+const testEctStatusEventListenerMethod = method => {
+  expect(method).toBeCalledTimes(1);
+  expect(method.mock.calls[0][0]).toEqual('change');
+  expect(method.mock.calls[0][1].constructor).toEqual(Function);
+};
+
+afterEach(() => {
+  Object.values(ectStatusListeners).forEach(listener => listener.mockClear());
+});
 
 describe('useNetworkStatus', () => {
-  const map = {};
-
-  const ectStatusListeners = {
-    addEventListener: jest.fn().mockImplementation((event, callback) => {
-      map[event] = callback;
-    }),
-    removeEventListener: jest.fn()
-  };
-
-  afterEach(() => {
-    Object.values(ectStatusListeners).forEach(listener => listener.mockClear());
-  });
-
-  /**
-   * Tests that addEventListener or removeEventListener was called during the
-   * lifecycle of the useEffect hook within useNetworkStatus
-   */
-  const testEctStatusEventListenerMethod = method => {
-    expect(method).toBeCalledTimes(1);
-    expect(method.mock.calls[0][0]).toEqual('change');
-    expect(method.mock.calls[0][1].constructor).toEqual(Function);
-  };
-
   test(`should return "false" for unsupported case`, () => {
-    const { result } = renderHook(() => useNetworkStatus());
+    jest.isolateModules(() => {
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() => useNetworkStatus());
 
-    expect(result.current.supported).toBe(false);
+      expect(result.current.supported).toBe(false);
+    });
   });
 
   test('should return initialEffectiveConnectionType for unsupported case', () => {
-    const initialEffectiveConnectionType = '4g';
+    jest.isolateModules(() => {
+      const initialEffectiveConnectionType = '4g';
 
-    const { result } = renderHook(() =>
-      useNetworkStatus(initialEffectiveConnectionType)
-    );
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() =>
+        useNetworkStatus(initialEffectiveConnectionType)
+      );
 
-    expect(result.current.supported).toBe(false);
-    expect(result.current.effectiveConnectionType).toBe(
-      initialEffectiveConnectionType
-    );
+      expect(result.current.supported).toBe(false);
+      expect(result.current.effectiveConnectionType).toBe(
+        initialEffectiveConnectionType
+      );
+    });
   });
 
   test('should return 4g of effectiveConnectionType', () => {
-    global.navigator.connection = {
-      ...ectStatusListeners,
-      effectiveType: '4g'
-    };
+    jest.isolateModules(() => {
+      global.navigator.connection = {
+        ...ectStatusListeners,
+        effectiveType: '4g'
+      };
 
-    const { result } = renderHook(() => useNetworkStatus());
-
-    testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
-    expect(result.current.supported).toBe(true);
-    expect(result.current.effectiveConnectionType).toEqual('4g');
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() => useNetworkStatus());
+  
+      testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
+      expect(result.current.supported).toBe(true);
+      expect(result.current.effectiveConnectionType).toEqual('4g');
+    })
   });
 
   test('should not return initialEffectiveConnectionType for supported case', () => {
-    const initialEffectiveConnectionType = '2g';
-    global.navigator.connection = {
-      ...ectStatusListeners,
-      effectiveType: '4g'
-    };
+    jest.isolateModules(() => {
+      global.navigator.connection = {
+        ...ectStatusListeners,
+        effectiveType: '4g'
+      };
 
-    const { result } = renderHook(() =>
-      useNetworkStatus(initialEffectiveConnectionType)
-    );
-
-    testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
-    expect(result.current.supported).toBe(true);
-    expect(result.current.effectiveConnectionType).toEqual('4g');
+      const initialEffectiveConnectionType = '2g';
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() =>
+        useNetworkStatus(initialEffectiveConnectionType)
+      );
+  
+      testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
+      expect(result.current.supported).toBe(true);
+      expect(result.current.effectiveConnectionType).toEqual('4g');
+    });
   });
 
   test('should update the effectiveConnectionType state', () => {
-    const { result } = renderHook(() => useNetworkStatus());
+    jest.isolateModules(() => {
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() => useNetworkStatus());
 
-    act(() =>
-      result.current.setNetworkStatus({ effectiveConnectionType: '2g' })
-    );
+      act(() =>
+        result.current.setNetworkStatus({ effectiveConnectionType: '2g' })
+      );
 
-    expect(result.current.effectiveConnectionType).toEqual('2g');
+      expect(result.current.effectiveConnectionType).toEqual('2g');
+    });
   });
 
   test('should update the effectiveConnectionType state when navigator.connection change event', () => {
-    global.navigator.connection = {
-      ...ectStatusListeners,
-      effectiveType: '2g'
-    };
+    jest.isolateModules(() => {
+      global.navigator.connection = {
+        ...ectStatusListeners,
+        effectiveType: '2g'
+      };
+  
+      const { useNetworkStatus } = require('./');
+      const { result } = renderHook(() => useNetworkStatus());
 
-    const { result } = renderHook(() => useNetworkStatus());
-    global.navigator.connection.effectiveType = '4g';
-    act(() => map.change());
+      global.navigator.connection.effectiveType = '4g';
 
-    expect(result.current.effectiveConnectionType).toEqual('4g');
+      act(() => map.change());
+  
+      expect(result.current.effectiveConnectionType).toEqual('4g');
+    })
   });
 
   test('should remove the listener for the navigator.connection change event on unmount', () => {
-    global.navigator.connection = {
-      ...ectStatusListeners,
-      effectiveType: '2g'
-    };
+    jest.isolateModules(() => {
+      global.navigator.connection = {
+        ...ectStatusListeners,
+        effectiveType: '2g'
+      };
 
-    const { unmount } = renderHook(() => useNetworkStatus());
-
-    testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
-    unmount();
-    testEctStatusEventListenerMethod(ectStatusListeners.removeEventListener);
+      const { useNetworkStatus } = require('./');
+      const { unmount } = renderHook(() => useNetworkStatus());
+      
+      testEctStatusEventListenerMethod(ectStatusListeners.addEventListener);
+      unmount();
+      testEctStatusEventListenerMethod(ectStatusListeners.removeEventListener);
+    })
   });
 });
