7f522e8d655b3407f62b255bf6824ead4bc78e8e
Raibima Putra <rayp1100@gmail.com>
2020-06-17 23:18:58 +0700

    Fix test setup, add support for async await

--- babel.config.js
+++ babel.config.js
@@ -17,5 +17,10 @@
 module.exports = {
   presets: [
     "@babel/preset-env"
-  ]
+  ],
+  env: {
+    test: {
+      presets: [["@babel/preset-env", { targets: { node: "current" }}]]
+    }
+  }
 };
